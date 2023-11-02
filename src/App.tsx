1c428bf4ca3c5419ed3c9ff65a222cfc2d5ec474
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-02 11:22:56 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-const */
-import states from './data/states.json';
-import sunset from './images/sunset.png';
+import states from "./data/states.json";
+import sunset from "./images/sunset.png";
 
 export const App = () => {
 	const title = "Vite React Site";
@@ -8,20 +8,23 @@ export const App = () => {
 	return (
 		<>
 			<h1>{title}</h1>
-			<img src={sunset}/>
+			{/* here is the sunset */}
+			<img src={sunset} />
 			<div className="highlighted">hello</div>
 			{userType === "admin" ? (
 				<div>this is the admin info</div>
 			) : (
 				<div>normal user info</div>
 			)}
-<hr/>
-<img src="images/code.png"/>
-<hr/>
+			<hr />
+			<img src="images/code.png" />
+			<hr />
 
 			<h2>German States</h2>
-			{states.map((state,index) => (
-				<div key={index}>{state} ({index})</div>
+			{states.map((state, index) => (
+				<div key={index}>
+					{state} ({index})
+				</div>
 			))}
 			<hr />
 			{/* {states.map(state => {
