c3ea0bf378ea2d06c41c4e0d351715eb1935c622
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-02 10:54:54 +0100

    conditionals

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+/* eslint-disable prefer-const */
 export const App = () => {
-  return (<div>hello</div>)
-}
+	const title = "Vite React Site";
+	let userType = "admin";
+	return (
+		<>
+			<h1>{title}</h1>
+			<div className="highlighted">hello</div>
+			{userType === "admin" ? (
+				<div>this is the admin info</div>
+			) : (
+				<div>normal user info</div>
+			)}
+		</>
+	);
+};
